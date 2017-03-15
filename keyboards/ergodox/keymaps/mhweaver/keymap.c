0ad5c736219869f60cd62b105d636de56b24f4a0
mhweaver <mhweaver@gmail.com>
2017-03-15 14:59:54 -0600

    Switched back to vi-style arrow keys. EZ style is nice for the CLI, but annoying for everything else

--- keyboards/ergodox/keymaps/mhweaver/keymap.c
+++ keyboards/ergodox/keymaps/mhweaver/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |----------+------+------+------+------+------|  L1  |           |  L1  |------+------+------+-------+------+--------|
  * | LShift   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .   |   /  | RShift |
  * `----------+------+------+------+------+-------------'           `-------------+------+------+-------+------+--------'
- *   |Ctrl/Esc| LGUI | LALT | Left | Right|                                       | Down |  Up  |NumLock| App  | RGUI |
+ *   |Ctrl/Esc| LGUI | LALT | Left | Right|                                       | Left | Down |   Up  | Right| RGUI |
  *   `------------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | Del  | Home |       | PgUp |Ctrl/Esc|
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
  *                                 |      |ace   |LeftM |       |RightM|        |      |
- *                                 `--------------------'       `----------------------'
+ *                               `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RBRC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
              MO(SYMB),    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSPC,
-                                  KC_DOWN,KC_UP,  KC_NLCK,KC_APP,           KC_RGUI,
+                                  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          KC_RGUI,
              KC_PGUP,          CTL_T(KC_ESC),
              KC_PGDN,
              LGUI(KC_E),KC_TAB,KC_ENT
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |       |      |      |      |      |                                       |      |    . |   0  |   =  |      |
+ *   |       |      |      |      |      |                                       |   0  |    0 |   .  |   =  |NumLck|
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |FLASH |       |FLASH |      |
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+                         KC_0,   KC_0,    KC_DOT,  KC_EQL,  KC_NLCK,
        RESET,   KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
