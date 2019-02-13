ffa8b9828f6bd02c105320f3ee72d059084128fa
mhweaver <mhweaver@gmail.com>
2019-02-13 10:34:49 -0700

    Updated Plover config to use Gemini PR

--- layouts/community/ergodox/mhweaver/keymap.c
+++ layouts/community/ergodox/mhweaver/keymap.c
@@ -2,6 +2,7 @@
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
+#include "keymap_steno.h"
 
 //#define NO_XSHIFT
 
@@ -144,45 +145,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* Steno for Plover
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |  6   |  7   |   8  |   9  |  0   |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   q  |   w  |   e  |   r  |   t  |------|           |------|  y   |  u   |   i  |   o  |  p   |   [    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   a  |   s  |   d  |   f  |   g  |      |           |      |  h   |  j   |   k  |   l  |  ;   |   '    |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |   c  |   v  |------|       |------|  n   |  m   |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
+ * ,--------------------------------------------------.      ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |      |      |      |      |      |      |      |   NC   |
+ * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
+ * |   FN   |   1  |   2  |   3  |   4  |   5  |      |      |      |  6   |  7   |   8  |   9  |  NA  |   NB   |
+ * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
+ * |        |   S  |   T  |   P  |   H  |   *  |------|      |------|  *   |  F   |   P  |   L  |  T   |   D    |
+ * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
+ * |        |   S  |   K  |   W  |   R  |   *  |      |      |      |  *   |  R   |   B  |   G  |  S   |   Z    |
+ * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                  |      |      |  PWR | RES1 | RES2 |
+ *   `----------------------------------'                                  `----------------------------------'
+ *                                        ,-------------.  ,-------------.
+ *                                        |      |      |  |      |      |
+ *                                 ,------|------|------|  |------+------+------.
+ *                                 |      |      |      |  |      |      |      |
+ *                                 |   A  |   O  |------|  |------|  E   |  U   |
+ *                                 |      |      |      |  |      |      |      |
+ *                                 `--------------------'  `--------------------'
  */
 
 [_PLOVER] = LAYOUT_ergodox(  // layout: layer 4: Steno for Plover
         // left hand
-        KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-        KC_NO,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,
-        KC_NO,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,
-        KC_NO,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_NO,
-        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                                      KC_FN4, KC_NO,
-                                           KC_NO,
-                                 KC_C,   KC_V,   KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        STN_FN,  STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  KC_TRNS,
+        KC_TRNS, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1,
+        KC_TRNS, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                      KC_TRNS, KC_TRNS,
+                                               KC_TRNS,
+                               STN_A, STN_O,   KC_TRNS,
         // right hand
-             KC_TRNS,  KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_TRNS,
-             KC_TRNS,   KC_6,    KC_7,    KC_8,   KC_9,    KC_0,   KC_TRNS,
-                        KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,
-               KC_NO,   KC_H,    KC_J,    KC_K,   KC_L, KC_SCLN,   KC_QUOT,
-                              KC_TRNS, KC_TRNS,  KC_NO,   KC_NO,     KC_NO,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, STN_NC,
+             KC_TRNS, STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,
+                      STN_ST4, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
+               KC_NO, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
+                               KC_NO,   KC_NO,   STN_PWR, STN_RE1, STN_RE2,
         KC_TRNS, KC_TRNS,
         KC_TRNS,
-        KC_NO,  KC_N,   KC_M
+        KC_TRNS, STN_E,   STN_U
 ),
 };
 
@@ -256,7 +257,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-
+	steno_set_mode(STENO_MODE_GEMINI);
 };
 
 
